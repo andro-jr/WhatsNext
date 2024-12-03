f63b49d9a7c61615e7e77a80818ee4487a46727d
andro-jr <prabin20panta@gmail.com>
2024-12-03 22:01:37 +0545

    add: @ global import alias

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 const page: React.FC = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  console.log("res :", await res.json());
-
   return <div>Cabins Page</div>;
 };
 
