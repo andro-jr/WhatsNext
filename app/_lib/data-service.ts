08e756507574c3ec1990a180508ee38d3802b612
andro-jr <prabin20panta@gmail.com>
2024-12-07 17:37:54 +0545

    add: not found handled

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,6 +1,7 @@
 import { eachDayOfInterval } from "date-fns";
 import supabase from "./supabase";
 import { Booking, Guest } from "../_types";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -17,6 +18,7 @@ export async function getCabin(id: string) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
