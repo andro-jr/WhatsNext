bfadedfe7db085ca01f66be9917b65e8ec60f62c
andro-jr <prabin20panta@gmail.com>
2024-12-07 21:33:47 +0545

    add: revalidation added

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -14,6 +14,15 @@ export const generateMetadata = async ({ params }: PageParamType) => {
   return {
     title: `Cabin ${name}`,
   };
+};
+
+export const generateStaticParams = async () => {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+  return ids;
 };
 
 export default async function Page({ params }: PageParamType) {
