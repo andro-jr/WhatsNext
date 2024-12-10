62242b79cc548e747014fa6e8ecc0278f75c3d8b
andro-jr <prabin20panta@gmail.com>
2024-12-10 23:21:45 +0545

    update: cabin details segregated into it's own component

--- app/_types/index.ts
+++ app/_types/index.ts
@@ -1,10 +1,11 @@
 export type Cabin = {
   id: string;
   name: string;
+  image: string;
+  description: string;
   maxCapacity: number;
   regularPrice: number;
   discount: number;
-  image: string;
 };
 
 export type Booking = {
--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,9 +1,7 @@
+import CabinDetails from "@/app/_components/CabinDetails";
 import CabinReservation from "@/app/_components/CabinReservation";
 import Spinner from "@/app/_components/Spinner";
-import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
 import { Suspense } from "react";
 
 interface PageParamType {
@@ -32,53 +30,11 @@ export const generateStaticParams = async () => {
 export default async function Page({ params }: PageParamType) {
   const { cabinId } = params;
   const cabin = await getCabin(cabinId);
-  const { name, maxCapacity, image, description } = cabin;
+  const { name } = cabin;
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
-      <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-        <div className="relative scale-[1.15] -translate-x-3">
-          <Image
-            src={image}
-            alt={`Cabin ${name}`}
-            fill
-            className="object-cover"
-          />
-        </div>
-
-        <div>
-          <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
-            Cabin {name}
-          </h3>
-
-          <p className="text-lg text-primary-300 mb-10">
-            <TextExpander>{description}</TextExpander>
-          </p>
-
-          <ul className="flex flex-col gap-4 mb-7">
-            <li className="flex gap-3 items-center">
-              <UsersIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                guests
-              </span>
-            </li>
-            <li className="flex gap-3 items-center">
-              <MapPinIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Located in the heart of the{" "}
-                <span className="font-bold">Dolomites</span> (Italy)
-              </span>
-            </li>
-            <li className="flex gap-3 items-center">
-              <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Privacy <span className="font-bold">100%</span> guaranteed
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <CabinDetails cabin={cabin} />
 
       <div>
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
