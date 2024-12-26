bccd1eaed8294aa8533b7502fa0da4f713dae6d7
andro-jr <prabin20panta@gmail.com>
2024-12-26 14:48:09 +0545

    feat: signout

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -9,7 +9,7 @@ const page: React.FC = async () => {
   const session = await auth();
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {session?.user?.name}
+      Welcome, {session?.user?.name?.split(" ")[0]}
     </h2>
   );
 };
