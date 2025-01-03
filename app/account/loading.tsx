a702376939e0cca4f8cd99796ee983624447e8fe
andro-jr <prabin20panta@gmail.com>
2025-01-03 14:30:00 +0545

    test:ssh test commit

--- app/account/loading.tsx
+++ app/account/loading.tsx
@@ -4,6 +4,7 @@ export default function Loading() {
   return (
     <div className="grid items-center justify-center">
       <Spinner />
+      {/* test ssh commit */}
       <p className="text-xl text-primary-200">Loading</p>
     </div>
   );
